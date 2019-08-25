505833b76102a05f601bcc255cd0b0a1b992f644
dustin <dustinshimono@gmail.com>
2019-08-25 19:59:44 -0400

    Add @Transactional to application services

--- src/main/kotlin/com/hexarchbootdemo/application/service/FindVoterService.kt
+++ src/main/kotlin/com/hexarchbootdemo/application/service/FindVoterService.kt
@@ -5,8 +5,10 @@ import com.hexarchbootdemo.application.port.input.FindVoterUseCase.FindByLastNam
 import com.hexarchbootdemo.application.port.output.persistence.FindVoterPort
 import com.hexarchbootdemo.domain.model.Voter
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class FindVotersService(val findVoterRepository: FindVoterPort) : FindVoterUseCase {
     override fun findByLastName(command: FindByLastNameQuery): List<Voter> {
         return findVoterRepository.findVotersByLastName(command)
--- src/main/kotlin/com/hexarchbootdemo/application/service/RegisterVoterService.kt
+++ src/main/kotlin/com/hexarchbootdemo/application/service/RegisterVoterService.kt
@@ -3,10 +3,12 @@ package com.hexarchbootdemo.application.service
 import com.hexarchbootdemo.application.port.input.RegisterVoterUseCase
 import com.hexarchbootdemo.application.port.output.persistence.RegisterVoterPort
 import org.springframework.stereotype.Service
-import java.util.UUID
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 
 @Service
+@Transactional
 class RegisterVoterService(val registerVoterRepository: RegisterVoterPort) : RegisterVoterUseCase {
 
     override fun registerVoter(registerVoterCommand: RegisterVoterUseCase.RegisterVoterCommand): UUID {
