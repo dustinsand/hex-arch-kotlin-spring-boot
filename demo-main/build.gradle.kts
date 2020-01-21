06cbcd145b72a82610e24d10b185871958695777
dustin <dustinshimono@gmail.com>
2020-01-20 20:24:33 -0500

    WIP to turn into multi module project

--- demo-main/build.gradle.kts
+++ demo-main/build.gradle.kts
@@ -2,12 +2,21 @@ description = "demo-main"
 
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring")
     id("org.springframework.boot")
 }
 
+val junitJupiterVersion: String by project
+val restAssuredVersion: String by project
+val junitPlatformRunnerVersion: String by project
+val assertjVersion: String by project
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(project(":demo-application"))
+    implementation(project(":adapter:input:rest"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:2.2.3.RELEASE")
+    implementation("com.h2database:h2:1.4.200")
 }
