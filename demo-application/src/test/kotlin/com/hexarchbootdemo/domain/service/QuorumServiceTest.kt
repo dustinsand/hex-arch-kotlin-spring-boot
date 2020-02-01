ac736ead3ebcec3ec679ac8407902352fdc17d52
dustin <dustinshimono@gmail.com>
2020-02-01 12:43:56 -0500

    Added CONTRIBUTING.MD

--- demo-application/src/test/kotlin/com/hexarchbootdemo/domain/service/QuorumServiceTest.kt
+++ demo-application/src/test/kotlin/com/hexarchbootdemo/domain/service/QuorumServiceTest.kt
@@ -1,10 +1,10 @@
 package com.hexarchbootdemo.domain.service
 
 import com.hexarchbootdemo.domain.model.Voter
+import java.util.Collections
+import java.util.UUID
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import java.util.Collections
-import java.util.*
 
 class QuorumServiceTest {
     @Test
