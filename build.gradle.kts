063f5d2547c009dac49eb3a1dbcd219bc988af38
dustin <dustinshimono@gmail.com>
2020-02-08 13:16:14 -0500

    Remove unnecessary dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,6 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-jooq:$springBootVersion")
         implementation("org.springframework.boot:spring-boot-starter-webflux:$springBootVersion")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonModuleKotlin")
-//        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
             exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
