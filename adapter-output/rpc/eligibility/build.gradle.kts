7e228ad6f592d398b4307e0e9dd8a62f441254c4
dustin <dustinshimono@gmail.com>
2020-04-19 12:02:33 -0400

    Added use of Resilience4j in adapter-output:rpc:eligibility

--- adapter-output/rpc/eligibility/build.gradle.kts
+++ adapter-output/rpc/eligibility/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 }
 
 dependencies {
+    val resilience4jVersion: String by project
+
     implementation(project(":voter-application-core"))
     implementation(project(":voter-common"))
+    implementation("io.github.resilience4j:resilience4j-spring-boot2:${resilience4jVersion}")
+    implementation("io.github.resilience4j:resilience4j-circuitbreaker:${resilience4jVersion}")
+    implementation("io.github.resilience4j:resilience4j-kotlin:${resilience4jVersion}")
 }
--- adapter-output/rpc/eligibility/src/main/java/com/hexarchbootdemo/adapter/output/rpc/eligibility/internal/VoterEligibilityRepository.kt
+++ adapter-output/rpc/eligibility/src/main/java/com/hexarchbootdemo/adapter/output/rpc/eligibility/internal/VoterEligibilityRepository.kt
@@ -2,6 +2,8 @@ package com.hexarchbootdemo.adapter.output.rpc.eligibility.internal
 
 import com.hexarchbootdemo.application.port.input.VoterEligibilityUseCase.VoterEligibilityQuery
 import com.hexarchbootdemo.application.port.output.repository.VoterEligibilityPort
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.kotlin.circuitbreaker.executeSuspendFunction
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -14,26 +16,47 @@ import org.springframework.web.reactive.function.client.awaitExchange
 internal class VoterEligibilityRepository : VoterEligibilityPort {
     /**
      * The intent of this function is to show an example of how to asynchronously call two remote services.
+     * Also, shows basic usage of Resilicen4j library for managing fault tolerance. The fault tolerance
+     * could be greatly enhanced here to take advantage of all the features in the library.
      *
      * This method ALWAYS returns true because it doesn't call real voter eligibility services.
      */
     override suspend fun isEligible(query: VoterEligibilityQuery): Boolean = coroutineScope {
-        val federalCheck: Deferred<String> = async {
-            val webClient = WebClient.create("https://google.com")
-            webClient.get()
-                    .uri("/")
-                    .awaitExchange()
-                    .awaitBody<String>()
+        var callCountGoogle = 0
+        val circuitBreakerGoogle = CircuitBreaker.ofDefaults("google")
+        circuitBreakerGoogle.eventPublisher.onSuccess {
+            println("Successfully called Google.")
+            callCountGoogle++
         }
-        val stateCheck: Deferred<String> = async {
-            val webClient = WebClient.create("https://kotlinlang.org")
-            webClient.get()
-                    .uri("/")
-                    .awaitExchange()
-                    .awaitBody<String>()
+        val federalCheck: Deferred<String> = circuitBreakerGoogle.executeSuspendFunction {
+            async {
+                val webClient = WebClient.create("https://google.com")
+                webClient.get()
+                        .uri("/")
+                        .awaitExchange()
+                        .awaitBody<String>()
+            }
+        }
+
+        var callCountKotlinlang = 0
+        val circuitBreakerKotlinlang = CircuitBreaker.ofDefaults("kotlinlang")
+        circuitBreakerKotlinlang.eventPublisher.onSuccess {
+            println("Successfully called Kotlinlang.")
+            callCountKotlinlang++
+        }
+        val stateCheck: Deferred<String> = circuitBreakerKotlinlang.executeSuspendFunction {
+            async {
+                val webClient = WebClient.create("https://kotlinlang.org")
+                webClient.get()
+                        .uri("/")
+                        .awaitExchange()
+                        .awaitBody<String>()
+            }
         }
 
         // Assert the web calls actually are returning a response
+        assert(callCountGoogle == 1)
+        assert(callCountKotlinlang == 1)
         assert(federalCheck.await().isNotEmpty())
         assert(stateCheck.await().isNotEmpty())
 
